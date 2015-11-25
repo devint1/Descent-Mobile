e217ac7ba2042b91f32011daa71c943218762e66
Devin Tuchsen <devint1@icloud.com>
2015-11-24 18:40:19 -0600

    Better way of saving screen to texture

--- 2D/GR.C
+++ 2D/GR.C
@@ -662,24 +662,19 @@ void ogles_draw_saved_screen(GLuint saved_screen_tex) {
 	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
 }
 
-// TODO: Use glCopyTexImage2D instead of this
 GLuint ogles_save_screen() {
-	GLubyte *saved_screen;
 	GLuint tex;
 	GLint viewWidth, viewHeight;
 	
 	getRenderBufferSize(&viewWidth, &viewHeight);
-	saved_screen = malloc(viewWidth * viewHeight * 4);
 	glEnable(GL_TEXTURE_2D);
-	glReadPixels(0, 0, viewWidth, viewHeight, GL_RGBA, GL_UNSIGNED_BYTE, saved_screen);
 	glGenTextures(1, &tex);
 	glBindTexture(GL_TEXTURE_2D, tex);
 	glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, viewWidth, viewHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, saved_screen);
-	free(saved_screen);
+	glCopyTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 0, 0, viewWidth, viewHeight, 0);
 	return tex;
 }
 #endif
